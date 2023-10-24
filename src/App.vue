3c756cf6e6521228aa55bcb933bd7e5223f955f8
Patricia Fiol <patricia.fiol@outlook.com>
2023-10-24 11:23:01 +0200

    project set-up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<header class="header">
+			<img class="logo" src="./assets/logo.png" alt="" />
+			<h1>Weather Forecast</h1>
+
+		</header>
+		<main>
+			<input id="search-bar" type="text">
+		</main>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	#app {
+		display: flex;
+		flex-direction: column;
+		color: #2c3e50;
+		text-align: center;
+		min-height: 100vh;
+		background-image: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 0.15),
+			rgba(0, 0, 0, 0.75)
+		);
+	}
+
+	.header {
+		padding: 2rem;
+	}
+	.logo {
+		width: 6em;
+	}
+	h1 {
+		font-family: 'Paytone One', sans-serif;
+		font-size: 2.5rem;
+		letter-spacing: .1rem;
+		color: rgb(247, 243, 243);
+	}
+	input {
+		width: 60%;
+		height: 2em;
+		border-radius: .5em;
+		border: none;
+		padding: .5em;
+		margin-top: 2em;
+		box-shadow: 0 0 .5em rgb(247, 243, 243);
+	}
 </style>
